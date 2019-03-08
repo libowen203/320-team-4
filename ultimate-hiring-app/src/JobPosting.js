07d92bfda908fd895cea28f7c1c5276a14f0788b
Daniel Hannon <47189201+Daniel-Hannon@users.noreply.github.com>
2019-03-08 16:41:47 -0500

    Form added, table commented out
    I commented out the table we tested on and created the form. Right now it's pretty bare bones and as we move forward there are definitely some feilds that should be auto filled in, but for now it's working

--- ultimate-hiring-app/src/JobPosting.js
+++ ultimate-hiring-app/src/JobPosting.js
@@ -1,8 +1,15 @@
 import React, {Component} from 'react';
 
 class JobPosting extends Component{
+
+    handleSubmit(e) { //this is where the info is sent to when submt is hit
+        alert( "The value is "  + this.input.value);
+        e.preventDefault();
+    }
+
 	render() {
         return (
+            /*
             <table>
                 <thead>
                     <tr>
@@ -29,6 +36,44 @@ class JobPosting extends Component{
                     </tr>
                 </tbody>
             </table>
+            */
+
+            // testing putting up a form
+            //start of the form
+            <form onSubmit={this.handleSubmit}> 
+                <label>
+                    Posting_ID: 
+		            <input type="text" ref={(input) => this.input = input} />
+                </label>
+                <br></br>
+                <label>
+                    Manager_ID: 
+		            <input type="text" ref={(input) => this.input = input} />
+                </label>
+                <br></br>
+                <label>
+                    Job Title: 
+                    <input type="text" ref={(input) => this.input = input} />
+                </label>
+                <br></br>
+                <label>
+                    Job Description:<br></br>
+		            <textarea rows="5" cols="30"> </textarea>
+                </label>
+                <br></br>
+                <label>
+                    Company_ID: 
+		            <input type="text" ref={(input) => this.input = input} />
+                </label>
+                <br></br>
+                <label>
+                    Posting Date: 
+		            <input type="text" ref={(input) => this.input = input} />
+                </label>
+                <br></br>
+                <input type="submit" value="Submit" />
+            </form> 
+
         );
     }
 }
