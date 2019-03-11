4cd88877067f6a93a99d898862cbdb7d91423dc9
Andrew Cunningham <andrewcunnin@umass.edu>
2019-03-11 14:44:32 -0400

    add backend, remove mongo container

---
+++ ultimate-hiring-app/src/server/server.js
@@ -0,0 +1,24 @@
+/* **
+This server.js is based off of the tutorial at https://medium.com/javascript-in-plain-english/full-stack-mongodb-react-node-js-express-js-in-one-simple-app-6cc8ed6de274
+** */
+const mongoose = require('mongoose')
+const express = require('express')
+
+const app = express()
+const router = express.Router()
+
+const dbRoute = "../../data/db:27017"
+
+
+// connects our back end code with the database
+mongoose.connect(
+  dbRoute,
+  { useNewUrlParser: true }
+);
+
+let db = mongoose.connection
+
+db.once("open", () => console.log("connected"))
+
+app.listen('3000')
+console.log("Working")
