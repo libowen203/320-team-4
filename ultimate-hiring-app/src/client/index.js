67507acb5f685171b1440219d93b0261ebc86748
Daniel Hannon <47189201+Daniel-Hannon@users.noreply.github.com>
2019-03-22 15:02:27 -0400

    Names are now working
    Deleted second copy of index, fixed the path to manager

--- ultimate-hiring-app/src/client/index.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import App from './Pageviews/Manager';
-import './client/CSS/index.css';
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
