49164258b660361870837f1968591ffac07ad0a1
Daniel Hannon <47189201+Daniel-Hannon@users.noreply.github.com>
2019-03-22 15:32:57 -0400

    Created Landing page
    Created simple landing page with just a submit button and routed index.js to open to that page

---
+++ ultimate-hiring-app/src/client/Components/LogInbutton.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+
+class LogInbutton extends Component{
+    
+    testfunction(){
+        return(
+            <div>hello world</div>
+        )
+    }
+
+    render(){
+        return(
+    <button>
+>Log In </button>
+        );
+    }
+}
+
+export default LogInbutton;
