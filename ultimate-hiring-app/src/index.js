fefa73bb66e4dcfb21e8c5a5d485cc4f6c1de0ba
Mike Towle <m77towle@gmail.com>
2019-03-23 11:20:50 -0400

    Revert "Default Manager page"
    This reverts commit 4fa8429f1a03c74622a966e0e735188a507f8a4f.

--- ultimate-hiring-app/src/index.js
+++ ultimate-hiring-app/src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './client/Pageviews/Manager';
+import App from './client/Pageviews/DefaultView';
 import './client/CSS/index.css';
 
 
