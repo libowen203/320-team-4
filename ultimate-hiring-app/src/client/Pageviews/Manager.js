192fdc5de5aaaecdfe7fc8222c7af684f277a20e
Mike Towle <m77towle@gmail.com>
2019-03-23 11:20:39 -0400

    Revert "Update Manager.js"
    This reverts commit 0ebf5b8030a6396964700cafb7daefa11eff4b7f.

--- ultimate-hiring-app/src/client/Pageviews/Manager.js
+++ ultimate-hiring-app/src/client/Pageviews/Manager.js
@@ -1,13 +1,10 @@
 import React, {Component} from 'react';
 import JobPosting from '../Components/JobPosting';
-import ManagerDash from '../Components/ManagerDash'; //make sure to switch this back to JobPosting
-
 class App extends Component {
     render() {
         return (
             <div className="Postings">
-                <JobPosting />
-                <ManagerDash />
+                <JobPosting/>
             </div>
         );
     }
