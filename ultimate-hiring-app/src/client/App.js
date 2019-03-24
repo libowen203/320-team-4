1417dd7c27c15d60f242097c5327656588d24bde
Mike Towle <m77towle@gmail.com>
2019-03-23 20:14:39 -0400

    Change pageview to Manager

--- ultimate-hiring-app/src/client/App.js
+++ ultimate-hiring-app/src/client/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-import JobPosting from './Components/JobPosting';
+import Manager from './Pageviews/Manager';
 class App extends Component {
     render() {
         return (
             <div className="Postings">
-                <JobPosting/>
+                <Manager />
             </div>
         );
     }
