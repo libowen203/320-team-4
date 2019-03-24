1d710b55897aa22e099cad76b27487aeb260b8b2
Mike Towle <m77towle@gmail.com>
2019-03-23 20:16:07 -0400

    Reformatting

--- ultimate-hiring-app/src/client/Pageviews/DefaultView.js
+++ ultimate-hiring-app/src/client/Pageviews/DefaultView.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import LogInbutton from '../Components/LogInbutton';
+
 class App extends Component {
     render() {
         return (
             <div className="LogIn">
-                <LogInbutton/>
+                <LogInbutton />
             </div>
         );
     }
