2d265418d3dd0c5fd32e25cb6d1c128b3c5ae316
Mike Towle <m77towle@gmail.com>
2019-03-23 20:15:24 -0400

    Update JobPostingCard

--- ultimate-hiring-app/src/client/Components/JobPostingCard.js
+++ ultimate-hiring-app/src/client/Components/JobPostingCard.js
@@ -13,7 +13,7 @@ class JobPostingCard extends Component {
         <Card>
           <CardImg top src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
           <CardBody>
-            <CardTitle>Job Posting</CardTitle>
+            <CardTitle> Card Title goes here</CardTitle>
           </CardBody>
         </Card>
       </div>
