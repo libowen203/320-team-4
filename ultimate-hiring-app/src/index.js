8573873ad76f77cd1908fc39224de6351aed2aec
Mike Towle <m77towle@gmail.com>
2019-03-23 20:13:22 -0400

    Import stylesheet for grid layout

--- ultimate-hiring-app/src/index.js
+++ ultimate-hiring-app/src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './client/App';
 import './client/CSS/index.css';
+import './client/CSS/bootstrap/css/bootstrap-grid.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
