e8d5c2b1eaa2de2c442e65b0bb0021ba64fbe91a
Andrew Cunningham <andrewcunnin@umass.edu>
2019-03-25 15:08:18 -0400

    add jobview component to default layout

--- ultimate-hiring-app/src/client/Pageviews/DefaultView.js
+++ ultimate-hiring-app/src/client/Pageviews/DefaultView.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
 import LogInbutton from '../Components/LogInbutton';
+import JobView from '../Components/JobView';
 class App extends Component {
     render() {
         return (
+          <div class = "view">
+          <header>
+          {/*} Menu component should go here, with login option */}
             <div className="LogIn">
                 <LogInbutton/>
             </div>
+          </header>
+          <div className = "container">
+            <JobView/>
+          </div>
+          </div>
         );
     }
 }
