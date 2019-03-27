03b914a6d5b03729ad1f6bd3728d45036df059e9
Mike Towle <m77towle@gmail.com>
2019-03-27 13:03:23 -0400

    Button Fix

--- ultimate-hiring-app/src/client/Components/JobPostingCard.js
+++ ultimate-hiring-app/src/client/Components/JobPostingCard.js
@@ -17,7 +17,7 @@ class JobPostingCard extends Component {
           <CardImg top src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
           <CardBody>
             <CardTitle> Card Title goes here </CardTitle>
-            <Button>Button</Button>
+            <Button class="btn" >Button</Button>
           </CardBody>
         </Card>
       </div>
