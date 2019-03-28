c6f7e9d23cc942fcec679c930bbde22a2a929459
Mike Towle <m77towle@gmail.com>
2019-03-27 20:14:01 -0400

    Gets rid of card image and replaces with card body instead

--- ultimate-hiring-app/src/client/Components/JobPostingCard.js
+++ ultimate-hiring-app/src/client/Components/JobPostingCard.js
@@ -14,10 +14,10 @@ class JobPostingCard extends Component {
     return (
       <div>
         <Card>
-          <CardImg top src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
           <CardBody>
-            <CardTitle> Card Title goes here </CardTitle>
-            <Button class="btn" >Button</Button>
+            <CardTitle> Job Posting Card </CardTitle>
+            <CardBody> Card Body: Text about card goes here </CardBody>
+            <Button class="btn"> Button </Button>
           </CardBody>
         </Card>
       </div>
