519a54b240e7606b668378077381739cc4fa3f42
Mike Towle <m77towle@gmail.com>
2019-03-27 20:13:05 -0400

    Simple Refactoring

--- ultimate-hiring-app/src/client/Pageviews/ManagerView.js
+++ ultimate-hiring-app/src/client/Pageviews/ManagerView.js
@@ -4,19 +4,19 @@ import ManagerDash from '../Components/ManagerDash';
 import LogOutButton from '../Components/LogOutButton';
 
 class ManagerView extends Component{
-    render(){
-       return (
-       <div>
-         <header>
-            <LogOutButton/>
-        </header> 
-       <div className="Postings">
-       <JobPosting />
-       <ManagerDash />
-       <br></br>
-       <br></br>
-       </div>
-       </div>
+  render(){
+    return (
+      <div>
+        <header>
+          <LogOutButton/>
+        </header>
+        <div className="Postings">
+          <JobPosting />
+          <ManagerDash />
+          <br></br>
+          <br></br>
+        </div>
+      </div>
       );
     }
 }
