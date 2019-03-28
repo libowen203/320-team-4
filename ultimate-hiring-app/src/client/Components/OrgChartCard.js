d10224f00eced7352affd5c09cfb08370192e9fa
Mike Towle <m77towle@gmail.com>
2019-03-27 20:13:15 -0400

    Create OrgChartCard.js

---
+++ ultimate-hiring-app/src/client/Components/OrgChartCard.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+class OrgChartCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPopup: false
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Card>
+          <CardBody>
+            <CardTitle> Org Chart Card </CardTitle>
+            <CardBody> Card Body: Text about card goes here </CardBody>
+            <Button class="btn"> Button </Button>
+          </CardBody>
+        </Card>
+      </div>
+    )
+  }
+}
+
+export default OrgChartCard;
