573189934750edb14c44497d5ddce68950033015
Daniel Hannon <47189201+Daniel-Hannon@users.noreply.github.com>
2019-04-01 15:06:33 -0400

    Update Menu.js

--- ultimate-hiring-app/src/client/Components/Menu.js
+++ ultimate-hiring-app/src/client/Components/Menu.js
@@ -2,15 +2,28 @@ import React, {Component} from 'react';
 import {Navbar, NavbarBrand, Nav, NavItem, NavLink} from 'reactstrap';
 
 class Menu extends Component {
+  constructor(props){
+    super(props);
+    this.state= {notLoggedIn:true}
+  }
+
   render() {
     return (
       <div>
         <Navbar color="light">
           <NavbarBrand> Ultimate Hiring App</NavbarBrand>
           <Nav className="ml-auto" navbar>
+          { notLoggedIn &&
             <NavItem>
-              <NavLink href="/managerview"> Log in </NavLink>
+            <NavLink href="/managerview"> Log in </NavLink>
             </NavItem>
+          }
+          { !notLoggedIn &&
+            <NavItem>
+            <NavLink href="/"> Log out </NavLink>
+            </NavItem>
+          }
+            
           </Nav>
         </Navbar>
       </div>
