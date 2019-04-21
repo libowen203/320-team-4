d1d8301f42bb653f33ef05c25183d6c14a6833b4
Maxwell Hubbard <hubs.maxwell@gmail.com>
2019-04-21 12:16:45 -0400

    Fixed CSS and sizing of Job Applying popup
    Also fixed stylings for Job landing page so that cards aren't weirdly sized

--- ultimate-hiring-app/src/client/Components/CurrentJobPostingCards.js
+++ ultimate-hiring-app/src/client/Components/CurrentJobPostingCards.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-import axios from 'axios/index'
-import { Container, Row, Col, Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 import JobPostingCard from "./JobPostingCard";
 
 //Hardcoded data (Should be replaced by data from backend)
@@ -14,11 +13,13 @@ class CurrentJobPostingCards extends Component {
     super(props);
     console.log(this.props);
     this.state = {
-      jobsList: this.props.jobs,
+      jobsList: this.props.jobs.reverse(),
       currentPage: 1,
       jobsPerPage: 6
     };
     this.handleClick = this.handleClick.bind(this);
+    this.toStart = this.toStart.bind(this);
+    this.toLast = this.toLast.bind(this);
   }
 
   //Handle clicks when navigation buttons are pressed
@@ -28,78 +29,122 @@ class CurrentJobPostingCards extends Component {
     });
   }
 
+  toStart(event) {
+      this.setState({
+          currentPage: 1
+      });
+  }
+
+
 
   render() {
-    const { jobsList: jobs, currentPage, jobsPerPage } = this.state;
+      const {jobsList: jobs, currentPage, jobsPerPage} = this.state;
 
-    // Logic for displaying current jobPostings
-    const indexOfLastJobPosting = currentPage * jobsPerPage;
-    const indexOfFirstJobPosting = indexOfLastJobPosting - jobsPerPage;
-    const currentJobPosting = jobs.slice(indexOfFirstJobPosting, indexOfLastJobPosting);
-
-  
-
-    //Logic for displaying 6 cards per page
-    let JobPostingCardArray = currentJobPosting.map(function (object, index) {
-      console.log(index);
-if(index %6===0){
-  console.log("Using " + index);
-  return (
-      <div>
-    <Row>
-          <Col sm="6" md={{size: 10, offset: 1}}>
-            <Row>
-              <Col> <JobPostingCard title ={jobs[(currentPage -1)*6 + index%6].title} location ={jobs[(currentPage -1)*6 + index%6].location} description= {jobs[(currentPage -1)*6 + index%6].description}/> </Col>
-              <Col> <JobPostingCard invisible={(currentPage - 1) * 6 + (index + 1) % 6 >= jobs.length} title ={(currentPage -1)*6 + (index+1)%6 < jobs.length ? jobs[(currentPage -1)*6 + (index+1)%6].title : ""} location ={(currentPage -1)*6 + (index+1)%6 < jobs.length ? jobs[(currentPage -1)*6 + (index+1)%6].location : ""} description= {(currentPage -1)*6 + (index+1)%6 < jobs.length ?jobs[(currentPage -1)*6 + (index+1)%6].description : ""}/> </Col>
-              <Col> <JobPostingCard invisible={(currentPage - 1) * 6 + (index + 2) % 6 >= jobs.length} title ={(currentPage -1)*6 + (index+2)%6 < jobs.length ? jobs[(currentPage -1)*6 + (index+2)%6].title : ""} location ={(currentPage -1)*6 + (index+2)%6 < jobs.length ? jobs[(currentPage -1)*6 + (index+2)%6].location : ""} description= {(currentPage -1)*6 + (index+2)%6 < jobs.length ? jobs[(currentPage -1)*6 + (index+2)%6].description : ""}/> </Col>
-            </Row>
-            <br></br>
-            <Row>
-            <Col> <JobPostingCard invisible={(currentPage - 1) * 6 + (index + 3) % 6 >= jobs.length} title ={(currentPage -1)*6 + (index+3)%6 < jobs.length ? jobs[(currentPage -1)*6 + (index+3)%6].title : ""} location ={(currentPage -1)*6 + (index+3)%6 < jobs.length ? jobs[(currentPage -1)*6 + (index+3)%6].location : ""} description= {(currentPage -1)*6 + (index+3)%6 < jobs.length ? jobs[(currentPage -1)*6 + (index+3)%6].description : ""}/> </Col>
-            <Col> <JobPostingCard invisible={(currentPage - 1) * 6 + (index + 4) % 6 >= jobs.length} title ={(currentPage -1)*6 + (index+4)%6 < jobs.length ? jobs[(currentPage -1)*6 + (index+4)%6].title : ""} location ={(currentPage -1)*6 + (index+4)%6 < jobs.length ? jobs[(currentPage -1)*6 + (index+4)%6].location : ""} description= {(currentPage -1)*6 + (index+4)%6 < jobs.length ? jobs[(currentPage -1)*6 + (index+4)%6].description : ""}/> </Col>
-            <Col> <JobPostingCard invisible={(currentPage - 1) * 6 + (index + 5) % 6 >= jobs.length} title ={(currentPage -1)*6 + (index+5)%6 < jobs.length ? jobs[(currentPage -1)*6 + (index+5)%6].title : ""} location ={(currentPage -1)*6 + (index+5)%6 < jobs.length ? jobs[(currentPage -1)*6 + (index+5)%6].location : ""} description= {(currentPage -1)*6 + (index+5)%6 < jobs.length ? jobs[(currentPage -1)*6 + (index+5)%6].description : ""}/> </Col>
-
-            </Row>
-          </Col>
-        </Row>
-</div>)
- } 
+      // Logic for displaying current jobPostings
+      const indexOfLastJobPosting = currentPage * jobsPerPage;
+      const indexOfFirstJobPosting = indexOfLastJobPosting - jobsPerPage;
+      const currentJobPosting = jobs.slice(indexOfFirstJobPosting, indexOfLastJobPosting);
 
 
+      //Logic for displaying 6 cards per page
+      let JobPostingCardArray = currentJobPosting.map(function (object, index) {
+          if (index % 6 === 0) {
+              return (
+                  <div>
+                      <div className="row">
+                          <div className="c1"><JobPostingCard title={jobs[(currentPage - 1) * 6 + index % 6].title}
+                                                              location={jobs[(currentPage - 1) * 6 + index % 6].location}
+                                                              description={jobs[(currentPage - 1) * 6 + index % 6].description}
+                                                              job={jobs[(currentPage - 1) * 6 + index % 6]}/></div>
+                          <div className="c2"><JobPostingCard
+                              invisible={(currentPage - 1) * 6 + (index + 1) % 6 >= jobs.length}
+                              title={(currentPage - 1) * 6 + (index + 1) % 6 < jobs.length ? jobs[(currentPage - 1) * 6 + (index + 1) % 6].title : ""}
+                              location={(currentPage - 1) * 6 + (index + 1) % 6 < jobs.length ? jobs[(currentPage - 1) * 6 + (index + 1) % 6].location : ""}
+                              description={(currentPage - 1) * 6 + (index + 1) % 6 < jobs.length ? jobs[(currentPage - 1) * 6 + (index + 1) % 6].description : ""}
+                              job={jobs[(currentPage - 1) * 6 + (index + 1) % 6]}/></div>
+                          <div className="c3"><JobPostingCard
+                              invisible={(currentPage - 1) * 6 + (index + 2) % 6 >= jobs.length}
+                              title={(currentPage - 1) * 6 + (index + 2) % 6 < jobs.length ? jobs[(currentPage - 1) * 6 + (index + 2) % 6].title : ""}
+                              location={(currentPage - 1) * 6 + (index + 2) % 6 < jobs.length ? jobs[(currentPage - 1) * 6 + (index + 2) % 6].location : ""}
+                              description={(currentPage - 1) * 6 + (index + 2) % 6 < jobs.length ? jobs[(currentPage - 1) * 6 + (index + 2) % 6].description : ""}
+                              job={jobs[(currentPage - 1) * 6 + (index + 2) % 6]}/></div>
+                          <br/>
+                          <div className="c4"><JobPostingCard
+                              invisible={(currentPage - 1) * 6 + (index + 3) % 6 >= jobs.length}
+                              title={(currentPage - 1) * 6 + (index + 3) % 6 < jobs.length ? jobs[(currentPage - 1) * 6 + (index + 3) % 6].title : ""}
+                              location={(currentPage - 1) * 6 + (index + 3) % 6 < jobs.length ? jobs[(currentPage - 1) * 6 + (index + 3) % 6].location : ""}
+                              description={(currentPage - 1) * 6 + (index + 3) % 6 < jobs.length ? jobs[(currentPage - 1) * 6 + (index + 3) % 6].description : ""}
+                              job={jobs[(currentPage - 1) * 6 + (index + 3) % 6]}/></div>
+                          <div className="c5"><JobPostingCard
+                              invisible={(currentPage - 1) * 6 + (index + 4) % 6 >= jobs.length}
+                              title={(currentPage - 1) * 6 + (index + 4) % 6 < jobs.length ? jobs[(currentPage - 1) * 6 + (index + 4) % 6].title : ""}
+                              location={(currentPage - 1) * 6 + (index + 4) % 6 < jobs.length ? jobs[(currentPage - 1) * 6 + (index + 4) % 6].location : ""}
+                              description={(currentPage - 1) * 6 + (index + 4) % 6 < jobs.length ? jobs[(currentPage - 1) * 6 + (index + 4) % 6].description : ""}
+                              job={jobs[(currentPage - 1) * 6 + (index + 4) % 6]}/></div>
+                          <div className="c6"><JobPostingCard
+                              invisible={(currentPage - 1) * 6 + (index + 5) % 6 >= jobs.length}
+                              title={(currentPage - 1) * 6 + (index + 5) % 6 < jobs.length ? jobs[(currentPage - 1) * 6 + (index + 5) % 6].title : ""}
+                              location={(currentPage - 1) * 6 + (index + 5) % 6 < jobs.length ? jobs[(currentPage - 1) * 6 + (index + 5) % 6].location : ""}
+                              description={(currentPage - 1) * 6 + (index + 5) % 6 < jobs.length ? jobs[(currentPage - 1) * 6 + (index + 5) % 6].description : ""}
+                              job={jobs[(currentPage - 1) * 6 + (index + 5) % 6]}/></div>
 
-  });
-    
+                      </div>
 
-    // Logic for displaying page numbers
-    const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(jobs.length / jobsPerPage); i++) {
-      pageNumbers.push(i);
-    }
+                  </div>)
+          }
 
-    const renderPageNumbers = pageNumbers.map(number => {
+
+      });
+
+
+      // Logic for displaying page numbers
+      const pageNumbers = [];
+      for (let i = 1; i <= Math.ceil(jobs.length / jobsPerPage); i++) {
+          pageNumbers.push(i);
+      }
+
+      const renderPageNumbers = pageNumbers.map(number => {
+          return (
+              <div>
+              <PaginationItem>
+                  <PaginationLink
+                      key={number}
+                      id={number}
+                      onClick={this.handleClick}
+                  >
+                      {number}
+                  </PaginationLink>
+              </PaginationItem>
+              </div>
+          );
+      });
+
       return (
-        <li
-          key={number}
-          id={number}
-          onClick={this.handleClick}
-        >
-          {number}
-        </li>
+          <div>
+              {JobPostingCardArray}
+              <br/>
+              <Pagination aria-label="Page navigation" style={{position: "absolute", top: "90%", padding: "10px"}}>
+                  <PaginationItem>
+                      <PaginationLink previous onClick={this.toStart}/>
+                  </PaginationItem>
+                  {
+                      renderPageNumbers
+                  }
+                  <PaginationItem>
+                      <PaginationLink next onClick={this.toLast}/>
+                  </PaginationItem>
+              </Pagination>
+          </div>
+
       );
-    });
-
-    return (
-      <Container fluid>
-        {JobPostingCardArray}
-        <ul id="page-numbers">
-          {renderPageNumbers}
-        </ul>
-        </Container>
-
-    );
   }
-}
 
+    toLast(event) {
+        this.setState({
+            currentPage: Math.ceil(this.state.jobsList.length / 6)
+        });
+    }
+}
 
 
 
--- ultimate-hiring-app/src/client/Components/JobPostingPopup.js
+++ ultimate-hiring-app/src/client/Components/JobPostingPopup.js
@@ -1,97 +1,124 @@
 import React, {Component} from 'react';
 import axios from 'axios'
+import Input from "reactstrap/es/Input";
+import Button from "reactstrap/es/Button";
+import Label from "reactstrap/es/Label";
 
 
+class JobPostingPopup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fname: '',
+            lname: '',
+            email: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.togglePopup = this.togglePopup.bind(this);
 
-class JobPostingPopup extends Component{
-  constructor(props){
-      super(props);
-      this.state = {
-          fname: '',
-          lname: '',
-      };
-      this.handleSubmit = this.handleSubmit.bind(this);
-      this.togglePopup = this.togglePopup.bind(this);
-
-  }
+    }
 
 //Method to toggle popup window..
-  togglePopup() {
-    console.log("TOGGLE")
-    this.setState({
-      showPopup: !this.state.showPopup
-    });
-  }
+    togglePopup() {
+        this.setState({
+            showPopup: !this.state.showPopup
+        });
+    }
 
 //Method for action after hitting the submit button
     handleSubmit = (event) => {
         event.preventDefault();
-        let form = document.forms.postingInfo;
-        console.log(form.jobTitle.value)
+        let errorLabel = this.refs.errorLabel;
+        let form = document.forms.namedItem("postingInfo");
         //format data according to data dump
         //NOTE: data dump doesn't include posting ID. I'm wondering if we need it or if we'll always query by manager id and position title
         //for example, an employee wouldn't apply for a posting. They'd apply to a title. Then positions would be filled as they're filled
-        this.setState({
-          fname: form.fname.value,
-          lname: form.lname.value,
-      },() => { //callback param ensures that setstate occurs before post
-        //push data via backend
-        console.log('POST')
-        axios.post('http://localhost:3001/putData', this.state)
-        this.props.closePopup()
-      });
-      //document.getElementById("posting-form").reset();
 
-      }
+        var valid = true;
+        if (form.fname.value.trim() === "") {
+            errorLabel.className = "invalid";
+            errorLabel.innerHTML = "Please enter your first name";
+            valid = false;
+        } else
+        if (form.lname.value.trim() === "") {
+            errorLabel.className = "invalid";
+            errorLabel.innerHTML = "Please enter your last name";
+            valid = false;
+        } else
+        if (form.email.value.trim() === "" || !this.validateEmail(form.email.value)) {
+            errorLabel.className = "invalid";
+            errorLabel.innerHTML = "Please enter a valid email";
+            valid = false;
+        }
 
 
-  render(){
+        if (valid) {
 
-      return(
-        //Form elements with labels and inputs for job title and job description
-        <div>
-      return (
-      <div className='popup'>
-        <div className='popup_inner'>
-        <form name="postingInfo" onSubmit={this.handleSubmit} id="posting-form">
-          <div className="header">Apply For Job</div>
-            <div className="form-group">
-              <input type="text" name="fname" placeholder="First Name" value ={this.state.text}/>
-              <input type="text" name="lname" placeholder="Last Name" value ={this.state.text}/>
-              <br></br>
-              <input type="submit" value="Submit Application" />
-              <button id ="closeButton" onClick={this.props.closePopup}>Close</button>
+            this.setState({
+                fname: form.fname.value,
+                lname: form.lname.value,
+                email: form.email.value
+            }, () => { //callback param ensures that setstate occurs before post
+                //push data via backend
+                console.log('POST');
+                axios.post('http://localhost:3001/putData', this.state);
+                this.props.closePopup();
+            });
+        }
+        //document.getElementById("posting-form").reset();
+
+    };
+
+    validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+
+    render() {
+        console.log(this.props.job);
+        return (
+            //Form elements with labels and inputs for job title and job description
+            <div>
+                <div className='popup'>
+                    <div className='popup_inner'>
+                        <form name="postingInfo" onSubmit={this.handleSubmit} id="posting-form">
+                            <div className="header">
+                                <div className="vertical-center horizontal-center">Apply For {this.props.job.title}</div>
+                            </div>
+                            <div className="form-group">
+                                <div >
+                                <Label>First Name<span className="required-Field">*</span></Label>
+                                <Input type="text" name="fname" placeholder="First Name" value={this.state.text}/>
+                                <br/>
+                                <Label>Last Name<span className="required-Field">*</span></Label>
+                                <Input type="text" name="lname" placeholder="Last Name" value={this.state.text}/>
+                                <br/>
+                                <Label>Email<span className="required-Field">*</span></Label>
+                                <Input type="email" name="email" placeholder="tblessing@umass.edu" value={this.state.text}/>
+                                <br/>
+                                </div>
+                            </div>
+                            <div className="bottomButtons">
+                                <label ref="errorLabel" className="valid"> </label>
+                                <div className="row horizontal-center">
+                                    <div className="col text-right">
+                                        <Button id="submitButton">Submit Application</Button>
+                                    </div>
+                                    <div className="col text-left">
+                                        <Button id="closeButton" onClick={this.props.closePopup}>Close</Button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-          </form>
-        </div>
-      </div>
-    );
-    </div>
 
-  );
-  }
+        );
+    }
 
 
-
-
-}
-
-class Popup extends React.Component {
-
-  render() {
-
-    return (
-      <div className='popup'>
-        <div className='popup_inner'>
-        <h1>Posting Preview</h1>
-          <h2>{this.props.jobTitle}</h2>
-          <h4><b>First Name: </b> {this.props.fname}</h4>
-          <h4><b>Last Name: </b>{this.props.lname}</h4>
-          <button id ="closeButton" onClick={this.props.closePopup}>Done</button>
-        </div>
-      </div>
-    );
-  }
 }
 
 
