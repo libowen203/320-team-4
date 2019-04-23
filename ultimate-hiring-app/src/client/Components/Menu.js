88109952ea6b42cd95706938a0978e823ab1728e
Daniel Hannon <47189201+Daniel-Hannon@users.noreply.github.com>
2019-04-23 17:46:11 -0400

    Added buttons to the Menu
    Added Buttons that can change logged in state. They still need to be improved to redirect. Links in the menu are left

--- ultimate-hiring-app/src/client/Components/Menu.js
+++ ultimate-hiring-app/src/client/Components/Menu.js
@@ -1,29 +1,39 @@
 import React, {Component} from 'react';
 import {Navbar, NavbarBrand, Nav, NavItem, NavLink} from 'reactstrap';
+import {Redirect} from "react-router-dom";
+import {Button } from 'reactstrap';
 
 class Menu extends Component {
   constructor(props){
     super(props);
     this.state= {LoggedIn:false}
   }
+  
+  toggleSignIn() {
+    
+    this.setState({
+      LoggedIn: !this.state.LoggedIn
+    });
 
+  }
   render() {
     return (
         <div>
           <Navbar style={{backgroundColor: '#519e2f'}}>
             <NavbarBrand style={{color: 'white'}}> Ultimate Hiring App</NavbarBrand>
             <Nav className="ml-auto" navbar>
-              {this.LoggedIn ? (
+              {this.state.LoggedIn ? (
                   <NavItem>
-                    <NavLink href="/" onClick={this.setState({LoggedIn: false})}> Log Out </NavLink>
+                    <NavLink href="/"> Log Out </NavLink>
+                    <Button class="btn" onClick={this.toggleSignIn.bind(this)}> Log Out </Button>
                   </NavItem>
               ) : (
-
                   <NavItem>
                     <NavLink href="/managerview" > Manager Log In </NavLink>
+                    <Button class="btn" onClick={this.toggleSignIn.bind(this)}> Manager Log In </Button>
                     <NavLink href="/employeeview" > Employee Log In </NavLink>
+                    <Button class="btn" onClick={this.toggleSignIn.bind(this)}> Employee Log In </Button>
                   </NavItem>
-
               )}
           </Nav>
         </Navbar>
